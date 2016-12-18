89d2c61201fa05182d807785d89852110f3451f5
Super-Baleine <paul.feuvraux@muonium.ch>
2016-12-18 02:59:49 +0100

    comments added

--- core/enc.js
+++ core/enc.js
@@ -1,19 +1,17 @@
 var enc_gcm = function(pass, plt){
-	var sALT = sjcl.random.randomWords(2);
+	var sALT = sjcl.random.randomWords(2); //2*4=16 bytes
 
-	var key = derivation(sALT, pass);
-	var plt = str(plt);
+	var key = derivation(sALT, pass); //key derivation process
+	var plt = str(plt); //turn the string into a bytes array
 
-	var initvector = sjcl.random.randomWords(4);
-	console.log(initvector);
+	var initvector = sjcl.random.randomWords(4); //4*4=16 bytes
 
-	var a = sjcl.random.randomWords(1);
-	console.log(a)
+	var a = sjcl.random.randomWords(1); //authentification data 4 bytes
 
 	var enc = new sjcl.cipher.aes(key);
-	var ciphered = sjcl.mode.gcm.encrypt(enc, plt, initvector, a, 128)
+	var ciphered = sjcl.mode.gcm.encrypt(enc, plt, initvector, a, 128) //encrypt it!
 
-	return packet(ciphered, sALT, a, initvector);
+	return packet(ciphered, sALT, a, initvector); //return a packet with the ct and its parameters
 }
 
 var packet = function(c, s, a, i){
@@ -21,17 +19,17 @@ var packet = function(c, s, a, i){
 	var s = sjcl.codec.hex.fromBits(s); //salt
 	var a = sjcl.codec.hex.fromBits(a); //authentification data
 	var i = sjcl.codec.hex.fromBits(i); //initialization vector
-	var packaged = c+":"+s+":"+a+":"+i;
+	var packaged = c+":"+s+":"+a+":"+i; //join it
 	return packaged;
 }
 
 
 var derivation = function(sALT, passwd){
-	var key = sjcl.misc.pbkdf2(passwd, sALT, 2000, 256);
-	return key;
+	var key = sjcl.misc.pbkdf2(passwd, sALT, 2000, 256); //2000: iteration for strengthen by factor
+	return key; //bytes array
 }
 
 var str = function(str){
-	var str = sjcl.codec.utf8String.toBits(str);
+	var str = sjcl.codec.utf8String.toBits(str); //we need to turn it into a bytes array
 	return str;
 }
